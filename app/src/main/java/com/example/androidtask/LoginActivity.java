b73b261dcd3c78c7f837aa1de5a339dad2e90689
车立钧 <927548154@qq.com>
2023-08-02 17:59:49 +0800

    first commit

---
+++ app/src/main/java/com/example/androidtask/LoginActivity.java
@@ -0,0 +1,123 @@
+package com.example.androidtask;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.text.InputType;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Boolean bPwdSwitch = false;
+    private EditText etPwd;
+    private EditText etAccount;
+    private CheckBox cbRememberPwd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_login);
+
+
+        ImageView ivPwdSwitch = findViewById(R.id.iv_pwd_switch);
+        etPwd = findViewById(R.id.et_pwd);
+        etAccount = findViewById(R.id.et_account);
+        cbRememberPwd = findViewById(R.id.cb_remember_pwd);
+        Button btLogin = findViewById(R.id.bt_login);
+
+
+        String spFileName = getResources()
+                .getString(R.string.shared_preferences_file_name);
+        String accountKey = getResources()
+                .getString(R.string.login_account_name);
+        String passwordKey = getResources()
+                .getString(R.string.login_password);
+        String rememberPasswordKey = getResources()
+                .getString(R.string.login_remember_password);
+
+        SharedPreferences spFile = getSharedPreferences(
+                spFileName,
+                MODE_PRIVATE);
+        String account = spFile.getString(accountKey, null);
+        String password = spFile.getString(passwordKey, null);
+        boolean rememberPassword = spFile.getBoolean(
+                rememberPasswordKey,
+                false);
+
+        if (account != null && !TextUtils.isEmpty(account)) {
+            etAccount.setText(account);
+        }
+
+        if (password != null && !TextUtils.isEmpty(password)) {
+            etPwd.setText(password);
+        }
+
+        if (rememberPassword) cbRememberPwd.setChecked(true);
+
+        btLogin.setOnClickListener(this);
+
+        ivPwdSwitch.setOnClickListener(view -> {
+            int context = String.valueOf(etPwd.getText()).length();
+            bPwdSwitch = !bPwdSwitch;
+            if (bPwdSwitch) {
+                ivPwdSwitch.setImageResource(R.drawable.baseline_visibility_24);
+                etPwd.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+            } else {
+                ivPwdSwitch.setImageResource(R.drawable.baseline_visibility_off_24);
+                etPwd.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD | InputType.TYPE_CLASS_TEXT);
+                etPwd.setTypeface(Typeface.DEFAULT);
+            }
+            etPwd.setSelection(context);
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        String spFileName = getResources()
+                .getString(R.string.shared_preferences_file_name);
+        String accountKey = getResources()
+                .getString(R.string.login_account_name);
+        String passwordKey = getResources()
+                .getString(R.string.login_password);
+        String rememberPasswordKey = getResources()
+                .getString(R.string.login_remember_password);
+
+        SharedPreferences spFile = getSharedPreferences(
+                spFileName,
+                Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = spFile.edit();
+
+        if (cbRememberPwd.isChecked()) {
+            String password = etPwd.getText().toString();
+            String account = etAccount.getText().toString();
+
+            editor.putString(accountKey, account);
+            editor.putString(passwordKey, password);
+            editor.putBoolean(rememberPasswordKey, true);
+            editor.apply();
+        } else {
+            editor.remove(accountKey);
+            editor.remove(passwordKey);
+            editor.remove(rememberPasswordKey);
+            editor.apply();
+        }
+
+        Intent intent =new Intent();
+        intent.setClass(LoginActivity.this,MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+}
