602b706fd72cf2dbe41235a82456f1a5562d7497
xnliang34 <lxning2003@gmail.com>
2023-08-27 20:36:42 +0800

    回调

--- app/src/main/java/com/example/androidtask/ShareListFragment.java
+++ app/src/main/java/com/example/androidtask/ShareListFragment.java
@@ -1,9 +1,12 @@
 package com.example.androidtask;
 
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
 
 public class ShareListFragment extends Fragment {
-    public ShareListFragment(){
-        super(R.layout.fragment_share_list);
-    }
 }
